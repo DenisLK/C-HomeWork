96659c068b06fe1e3691084fa0c07181a82e7fd8
Denis <denul73@gmail.com>
2022-11-20 13:05:38 +0300

    task4

---
+++ задача4/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа вывода четных чисел в дипазоне от 1 до указанного числа
+int i = 1;
+            Console.Write("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while(i <= n)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+;
+                i++;
+            }
