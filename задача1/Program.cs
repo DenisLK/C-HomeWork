301c612592f977e34d0a822b6a8b97bd6be878a7
Denis <denul73@gmail.com>
2022-11-20 11:32:46 +0300

    task1

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -1,10 +1,16 @@
-﻿//Программа для нахождения максимального из 3 чисел
-int a = 22, b = 3, c = 9;
+﻿//Программа для нахождения большего и меньшего числа из двух
+int a = 2, b = -38;
 int max = a;
-
+int min = b;
 if (a > max) max = a;
 if (b > max) max = b;
-if (c > max) max = c;
+
+
+if (a < min) min = a;
+if (b < min) min = b;
+
 
 Console.Write("max= ");
 Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
