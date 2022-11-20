d078b53920a2e843f050862caff9e954d81570d7
Denis <denul73@gmail.com>
2022-11-20 14:50:12 +0300

    task2

--- задача2/Program.cs
+++ задача2/Program.cs
@@ -1,8 +1,7 @@
 ﻿//Программа для нахождения максимального из 3 чисел
-int a = 44, b = 5, c = 78;
+int a = 84, b = 5, c = 78;
 int max = a;
 
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
