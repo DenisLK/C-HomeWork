798cd6d18d9387c79a7841bba2ec1b612dc747f1
Denis <denul73@gmail.com>
2022-11-20 11:24:28 +0300

    две задачи

---
+++ задача1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа для нахождения максимального из 3 чисел
+int a = 22, b = 3, c = 9;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
---
+++ задача2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа для нахождения максимального из 3 чисел
+int a = 2, b = 3, c = 7;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
