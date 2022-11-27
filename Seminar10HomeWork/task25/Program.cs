7ee29f2db6d53a9ac754d28b76eb2d3886c01da0
Denis <Denul73@gmail.com>
2022-11-27 14:58:10 +0300

    task25ok

---
+++ Seminar10HomeWork/task25/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Программа принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+void ReadInt(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("Введите число A ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+ReadInt(A, B);
